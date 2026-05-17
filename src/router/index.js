36e5c445214623ff811994e4aa34a55235f068b4
omarmeneam <o.meneam@gmail.com>
2026-05-17 14:10:31 +0800

    Task 3 - configure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,29 @@
-// Router configuration will go here
+import { createRouter, createWebHistory } from 'vue-router'
+import AddPersonView from '../views/AddPersonView.vue'
+import LastPersonView from '../views/LastPersonView.vue'
+import PersonListView from '../views/PersonListView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'AddPerson',
+    component: AddPersonView
+  },
+  {
+    path: '/last',
+    name: 'LastPerson',
+    component: LastPersonView
+  },
+  {
+    path: '/list',
+    name: 'PersonList',
+    component: PersonListView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
