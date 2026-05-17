b33297d9bfd0819c19ee816d31c9c4dada663481
omarmeneam <o.meneam@gmail.com>
2026-05-17 14:11:16 +0800

    Task 6 - use router-link

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,9 +1,9 @@
 <template>
   <aside class="sidebar">
     <div style="padding: 0 20px 20px; font-weight: bold; font-size: 1.2em;">Navigation</div>
-    <a href="#" class="menu-link">Add Person</a>
-    <a href="#" class="menu-link">Last Added</a>
-    <a href="#" class="menu-link">Person List</a>
+    <router-link to="/" class="menu-link">Add Person</router-link>
+    <router-link to="/last" class="menu-link">Last Added</router-link>
+    <router-link to="/list" class="menu-link">Person List</router-link>
   </aside>
 </template>
 
